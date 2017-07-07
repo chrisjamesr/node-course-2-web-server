37d8dbaf349762e5f1a24b29c15bd066d024418b
chrisjamesr <chrisjames.r@gmail.com>
2017-07-07 19:54:23 -0400

    Comment out maintenance route and remove duplicate code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
 app.use((req, res, next) => {
 	var now = new Date().toString();
 	var log = `${now}: ${req.method} ${req.url}`
@@ -19,20 +19,22 @@ app.use((req, res, next) => {
 	});
 	next();
 })
-app.use((req, res, next) => {
-	res.render('maintenance', {
-			pageError: 'Page Not Available'
-		});
-});
+
 app.use(express.static(__dirname + '/public'));
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance', {
+// 			pageError: 'Page Not Available'
+// 		});
+// });
 
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear()
 });
+
 hbs.registerHelper('screamIt', (text) => {
 	return text.toUpperCase();
 });
-
 
 app.get('/', (req,res) => {
 	// res.send('<h1>Hello Express!</h1)>');
